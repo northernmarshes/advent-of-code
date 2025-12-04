fce2832d2c292a91b492a97aa08c85beb2b3618f
northernmarshes <northernmarshes@gmail.com>
2025-12-04 09:37:36 +0100

    251204-01-done

--- 2025/01/01.py
+++ 2025/01/01.py
@@ -1,4 +1,4 @@
-sec = "sequences.txt"
+input = "sequences.txt"
 sample = "sample.txt"
 
 sequences = []
@@ -7,25 +7,35 @@ for i in range(100):
     dial_positions.append(i)
 zeros = 0
 
-with open(sample, "r") as file:
+with open(input, "r") as file:
     lines = file.readlines()
     for line in lines:
         sequences.append(line[:-1])
 
 previous = 50
+
 for sequence in sequences:
     if sequence[0] == "L":
-        new_step = dial_positions[previous - int(sequence[1:])]
-        print(new_step)
+        click = int(sequence[1:])
+        if click >= 100:
+            click = int(str(click)[1:])
+        move = previous - click
+        if move >= 100:
+            move = int(str(move)[1:])
+        new_step = dial_positions[move]
+        # print(new_step)
         previous = new_step
         if previous == 0:
             zeros = zeros + 1
     else:
-        move = previous + int(sequence[1:])
+        click = int(sequence[1:])
+        if click >= 100:
+            click = int(str(click)[1:])
+        move = previous + click
         if move >= 100:
             move = int(str(move)[1:])
-        new_step = move
-        print(new_step)
+        new_step = dial_positions[move]
+        # print(new_step)
         previous = new_step
         if previous == 0:
             zeros = zeros + 1
