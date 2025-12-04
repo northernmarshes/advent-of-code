299f5e1267e5468291dd24dcdfdab99f444008fa
northernmarshes <northernmarshes@gmail.com>
2025-12-04 08:51:03 +0100

    251204-smallsampleworking

--- 2025/01/01.py
+++ 2025/01/01.py
@@ -5,19 +5,28 @@ sequences = []
 dial_positions = []
 for i in range(100):
     dial_positions.append(i)
-zeros = []
+zeros = 0
 
 with open(sample, "r") as file:
     lines = file.readlines()
     for line in lines:
         sequences.append(line[:-1])
 
-index = dial_positions[50]
-
+previous = 50
 for sequence in sequences:
     if sequence[0] == "L":
-        index = index - int(sequence[1:])
-        print("left")
-        print(index)
+        new_step = dial_positions[previous - int(sequence[1:])]
+        print(new_step)
+        previous = new_step
+        if previous == 0:
+            zeros = zeros + 1
     else:
-        print("right")
+        move = previous + int(sequence[1:])
+        if move >= 100:
+            move = int(str(move)[1:])
+        new_step = move
+        print(new_step)
+        previous = new_step
+        if previous == 0:
+            zeros = zeros + 1
+print(f"The total number of zero positions is: {zeros}")
