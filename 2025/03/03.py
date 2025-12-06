2c2eb3247739f4e16489001c39930dda48a009b4
northernmarshes <northernmarshes@gmail.com>
2025-12-06 16:04:32 +0100

    251206-03-progress_02

--- 2025/03/03.py
+++ 2025/03/03.py
@@ -9,7 +9,6 @@ jolt_units = 0
 banks_joltage = []
 total_joltage = 0
 nine_batteries = []
-count = 0
 
 # File loading
 with open(sample, "r") as file:
@@ -17,24 +16,77 @@ with open(sample, "r") as file:
     for line in lines:
         banks.append(line[:-1])
 
+
 # Part one logic
-for bank in banks:
-    jolt_decimal = max(int(x) for x in list(bank[:-1]))
-    cut = list(bank).index(str(jolt_decimal))
-    second_half = list(bank[cut + 1 :])
-    jolt_units = max(int(x) for x in list(second_half))
-    joltage = int(str(jolt_decimal) + str(jolt_units))
-    banks_joltage.append(joltage)
-total_joltage = sum(banks_joltage)
+def part_01(data):
+    for bank in data:
+        jolt_decimal = max(bank[:-1])
+        cut = list(bank).index(str(jolt_decimal))
+        second_half = list(bank[cut + 1 :])
+        jolt_units = max(second_half)
+        joltage = int(str(jolt_decimal) + str(jolt_units))
+        banks_joltage.append(joltage)
+    total_joltage = sum(banks_joltage)
+    return total_joltage
+
 
 # Part two logic
-for bank in banks:
-    battery = max(int(x) for x in list(bank))
-    nine_batteries.append(battery)
-    list(bank).remove(str(battery))
+def part_02_indexes(data):
+    indexes = []
+    bank_indexes = []
+    for bank in data:
+        count = 0
+        bank_list = []
+        for num in str(bank):
+            bank_list.append(num)
+        bank_list_copy = bank_list.copy()
+        while count < 12:
+            index = bank_list.index(max(bank_list))
+            indexes.append(index)
+            bank_list[index] = str(0)
+            count += 1
+        bank_indexes.append(indexes)
+        indexes = []
+        print(
+            [varx[1] for varx in enumerate(bank_list_copy) if varx[0] in [bank_indexes]]
+        )
+    return bank_indexes
 
 
-print("nine batteries", nine_batteries)
+def part_02_values(data, ind):
+    pass
 
 
-print("Total output joltage is:", total_joltage)
+# Working logic
+# number = 811111111111119
+number = 234234234234278
+numberlist = []
+for num in str(number):
+    numberlist.append(num)
+biggest_num = []
+biggest_num_index = []
+indexes = []
+count = 0
+numberlist_pure = numberlist.copy()
+while count < 12:
+    index = numberlist.index(max(numberlist))
+    indexes.append(index)
+    numberlist[index] = str(0)
+    count += 1
+
+indexes_sorted = sorted(indexes)
+
+print("indexes", indexes)
+print("indexes sorted", indexes_sorted)
+
+
+jolt = ""
+for num in range(len(numberlist_pure)):
+    if num in indexes_sorted:
+        jolt += numberlist_pure[num]
+print("joltge is:", jolt)
+
+
+# print("Total output joltage from two batteries is:", part_01(banks))
+# print("Total output joltage from nine batteries is:", part_02(banks))
+print(part_02_indexes(banks))
