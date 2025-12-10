b4186c49357a1ea7b2dceabd0b08ab1a15e97664
northernmarshes <northernmarshes@gmail.com>
2025-12-10 16:59:28 +0100

    251210-04-part-two

--- 2025/04/04.py
+++ 2025/04/04.py
@@ -5,6 +5,7 @@ input = "input.txt"
 
 # File loading
 matrix = []
+pickedmap = []
 row = []
 with open(input, "r") as file:
     lines = file.readlines()
@@ -13,6 +14,12 @@ with open(input, "r") as file:
             row.append(char)
         matrix.append(row)
         row = []
+    for line in lines:
+        for char in line[:-1]:
+            row.append(char)
+        pickedmap.append(row)
+        row = []
+
 
 # Changing characters for 0 and 1
 for row in matrix:
@@ -21,6 +28,14 @@ for row in matrix:
             row[char] = 0
         if row[char] == "@":
             row[char] = 1
+
+# Clearing pickedmap
+for row in pickedmap:
+    for char in range(len(row)):
+        if row[char] == ".":
+            row[char] = 0
+        if row[char] == "@":
+            row[char] = 0
 
 
 # Part one logic
@@ -83,4 +98,98 @@ def part_01(data):
     return accessablePapers
 
 
-print(part_01(matrix))
+print(
+    "In part one", part_01(matrix), "rolls of paper that can be accessed by a forklift."
+)
+
+
+# Part two logic
+def part_02(data):
+    paperisavialable = True
+    collectedPapers_round = 0
+    collectedPapers_overall = 0
+
+    while paperisavialable:
+        for row in range(len(data)):
+            for index, value in enumerate(data[row]):
+                if (
+                    row == 0
+                    or row == len(data) - 1
+                    or index == 0
+                    or index == len(data[row]) - 1
+                ):
+                    new_neighbours = []
+                    # bottom right
+                    if index != len(data[index]) - 1 and row != len(data) - 1:
+                        new_neighbours.append(data[row + 1][index + 1])
+                    # bottom left
+                    if index != 0 and row != len(data) - 1:
+                        new_neighbours.append(data[row + 1][index - 1])
+                    # top right
+                    if index != len(data[index]) - 1 and row != 0:
+                        new_neighbours.append(data[row - 1][index + 1])
+                    # top left
+                    if index != 0 and row != 0:
+                        new_neighbours.append(data[row - 1][index - 1])
+                    # top
+                    if row != 0:
+                        new_neighbours.append(data[(row - 1)][index])
+                    # right
+                    if index != len(data[index]) - 1:
+                        new_neighbours.append(data[row][index + 1])
+                    # bottom
+                    if row != len(data) - 1:
+                        new_neighbours.append(data[row + 1][index])
+                    # left
+                    if index != 0:
+                        new_neighbours.append(data[row][index - 1])
+                else:
+                    new_neighbours = [
+                        # top
+                        data[row - 1][index],
+                        # top left
+                        data[row - 1][index - 1],
+                        # top right
+                        data[row - 1][index + 1],
+                        # right
+                        data[row][index + 1],
+                        # bottom
+                        data[row + 1][index],
+                        # bottom left
+                        data[row + 1][index - 1],
+                        # bottom right
+                        data[row + 1][index + 1],
+                        # left
+                        data[row][index - 1],
+                    ]
+                if value == 1 and sum(new_neighbours) < 4:
+                    collectedPapers_round += 1
+                    pickedmap[row][index] = -1
+
+        # Subtrackting picked papers
+        data = [
+            [data[i][j] + pickedmap[i][j] for j in range(len(data[0]))]
+            for i in range(len(data))
+        ]
+
+        # Clearing pickedmap
+        for row in pickedmap:
+            for char in range(len(row)):
+                if row[char] == -1:
+                    row[char] = 0
+
+        # Adding collected papers
+        if collectedPapers_round != 0:
+            collectedPapers_overall += collectedPapers_round
+            collectedPapers_round = 0
+        else:
+            break
+
+    return collectedPapers_overall
+
+
+print(
+    "In part two",
+    part_02(matrix),
+    "rolls of paper in total can be removed by the Elves and their forklifts.",
+)
