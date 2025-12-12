d6468c4af0fbb586ff626a68fb4cede2364392e8
northernmarshes <northernmarshes@gmail.com>
2025-12-12 14:39:29 +0100

    251212-06-init

---
+++ 2025/06/06.py
@@ -0,0 +1,11 @@
+# Input files
+sample_file = "sample.txt"
+input_file = "input.txt"
+
+# Variables
+
+# File loading and appending lists
+with open(sample_file, "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        pass
