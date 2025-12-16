cdac76bf255e94fbb0023c94fb708ccde17b6424
northernmarshes <northernmarshes@gmail.com>
2025-12-17 00:28:04 +0100

    251217-07-progress

--- 2025/07/07.py
+++ 2025/07/07.py
@@ -5,7 +5,7 @@ input_file = "input.txt"
 cols = []
 
 with open(sample_file, "r") as file:
-    lines = [char.replace(".", " ").replace("^", "0") for char in file.readlines()]
+    lines = [char.replace(".", " ") for char in file.readlines()]
     cols = list(map(list, zip(*lines)))[:][:-1]
 
 # Rotating the matrix
@@ -27,7 +27,7 @@ def part_01(data):
             for ci, col in enumerate(row):
                 if ri < len(data) - 1 and col == "|" and data[ri + 1][ci] == " ":
                     data[ri + 1][ci] = "|"
-                elif col == "0":
+                elif col == "^":
                     if data[ri - 1][ci] == "|":
                         data[ri][ci + 1] = "|"
                         data[ri][ci - 1] = "|"
@@ -37,20 +37,32 @@ def part_01(data):
     return split
 
 
-print(f"A tachyon beam is split a total of {part_01(cols)} times.")
+# print(f"A tachyon beam is split a total of {part_01(cols)} times.")
 
 
 def part_02(data):
     count = 0
-    while count < 5:
+    split = 0
+    while count < 12000:
         for ri, row in enumerate(data):
             for ci, col in enumerate(row):
                 if ri < len(data) - 1 and col == "|" and data[ri + 1][ci] == " ":
-                    pass
-                if col == "0":
+                    data[ri + 1][ci] = "|"
+                elif col == "^":
                     if data[ri - 1][ci] == "|":
-                        pass
+                        data[ri][ci + 1] = "|"
+                        data[ri][ci - 1] = "|"
+                        data[ri][ci] = "@"
+                        split += 1
             count += 1
 
+    # for r, row in enumerate(data):
+    #     for c, col in enumerate(data[r]):
+    #         if data[r][c] == "K":
+    #             del data[r]
 
-# print(f"A single tachyon particle end up on {part_02(cols)} timelines.")
+    for row in data:
+        print(row)
+
+
+print(f"A single tachyon particle end up on {part_02(cols)} timelines.")
