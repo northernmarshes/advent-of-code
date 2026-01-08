5c21157e826462abbce4649ff54ac3ee961fbf1e
northernmarshes <northernmarshes@gmail.com>
2026-01-08 15:47:41 +0100

    260108-y24-03-regex

--- 2024/03/03.py
+++ 2024/03/03.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 sample_file = "sample.txt"
 input_file = "input.txt"
@@ -11,23 +12,40 @@ with open(sample_file, "r") as f:
         calc_lines.append(line[:-1].split("mul"))
 
 
-def part_1(data):
+# def part_1(data):
+#     result = 0
+#     calculations = []
+#     for line in data:
+#         for calc in line:
+#             if "(" and ")" in calc:
+#                 calculations.append(calc)
+#
+#     multipliers = []
+#     for calc in calculations:
+#         print(calc)
+#         inside = calc[calc.index("(") + 1 : calc.index(")")]
+#         multipliers.append([int(x) for x in inside.split(",")])
+#
+#     for calc in multipliers:
+#         result += math.prod(calc)
+#     return result
+
+# print(part_1(calc_lines))
+
+
+def part_1_regex(data):
     result = 0
     calculations = []
+    pattern = re.compile(r"\(\d+,\d+\)")
     for line in data:
-        for calc in line:
-            if "(" and ")" in calc:
-                calculations.append(calc)
-
-    multipliers = []
+        for calc in line[1:]:
+            calculation = pattern.findall(calc)
+            if calculation:
+                calculations.append(calculation[0][1:-1])
     for calc in calculations:
-        inside = calc[calc.index("(") + 1 : calc.index(")")]
-        multipliers.append([int(x) for x in inside.split(",")])
-    print()
-
-    for calc in multipliers:
+        calc = [int(x) for x in calc.split(",")]
         result += math.prod(calc)
     return result
 
 
-print(part_1(calc_lines))
+print(part_1_regex(calc_lines))
