459afb72ee6f5f2754b8a68c122c4e7077441fa1
northernmarshes <northernmarshes@gmail.com>
2026-01-21 10:08:28 +0100

    260121-y24-05-wip1

--- 2024/05/05.py
+++ 2024/05/05.py
@@ -23,9 +23,17 @@ def parse_data(data: str):
 
 def part_01(data: str):
     """Part 1 logic"""
-    page_order = []
+    page_order = set()
     rules = (parse_data(data))[0]
     updates = (parse_data(data))[1]
+
+    all_pages = set()
+    for rule in rules:
+        for number in rule:
+            all_pages.add(number)
+    print(set(all_pages))
+    number_of_keys = len(set(all_pages))
+    print(number_of_keys)
 
 
 part_01(sample_file)
