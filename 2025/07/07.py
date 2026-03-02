a2b9f3b858b2b75a54b3f4ab8243379bb649145e
northernmarshes <northernmarshes@gmail.com>
2026-03-02 19:41:27 +0100

    260302-y25-07-part-2-done

--- 2025/07/07.py
+++ 2025/07/07.py
@@ -4,7 +4,7 @@ input_file = "input.txt"
 
 cols = []
 
-with open(sample_file, "r") as file:
+with open(input_file, "r") as file:
     lines = [char.replace(".", " ") for char in file.readlines()]
     cols = list(map(list, zip(*lines)))[:][:-1]
 
@@ -20,6 +20,7 @@ for ri, row in enumerate(cols):
 
 # Part one logic
 def part_01(data):
+    """Part 1 logic"""
     count = 0
     split = 0
     while count < 12000:
@@ -37,95 +38,82 @@ def part_01(data):
     return split
 
 
-# print(f"A tachyon beam is split a total of {part_01(cols)} times.")
+print(f"A tachyon beam is split a total of {part_01(cols)} times.")
 
 
-# def part_02(data):
-#     count = 0
-#     split = 0
-#     while count < 12000:
-#         for ri, row in enumerate(data):
-#             for ci, col in enumerate(row):
-#                 if ri < len(data) - 1 and col == "|" and data[ri + 1][ci] == " ":
-#                     data[ri + 1][ci] = "|"
-#                 elif col == "^":
-#                     if data[ri - 1][ci] == "|":
-#                         data[ri][ci + 1] = "|"
-#                         data[ri][ci - 1] = "|"
-#                         data[ri][ci] = "@"
-#                         split += 1
-#             count += 1
-#
-#     # Adding leaf nodes
-#     bottom_row = []
-#     for r in range(len(data[0])):
-#         bottom_row.append(" ")
-#     data.append(bottom_row)
-#
-#     for r, row in enumerate(data):
-#         for c, col in enumerate(data[r]):
-#             if r == len(data) - 1 and data[r - 1][c] == "|":
-#                 data[r][c] = "@"
-#
-#     # Initial count
-#     for ri, row in enumerate(cols):
-#         for ci, col in enumerate(row):
-#             if ri == 2 and col == "@":
-#                 cols[ri][ci] = "1"
-#
-#     # Counting ways to each node
-#     for r, row in enumerate(data):
-#         for c, col in enumerate(row):
-#             if data[r][c] == "@":
-#                 node_paths = 0
-#                 if (
-#                     data[r - 2][c - 1] != " "
-#                     and data[r - 2][c - 1] != "|"
-#                     and data[r - 2][c - 1] != "^"
-#                 ):
-#                     node_paths += int(data[r - 2][c - 1])
-#                 if (
-#                     c < len(row) - 1
-#                     and data[r - 2][c + 1] != " "
-#                     and data[r - 2][c + 1] != "|"
-#                     and data[r - 2][c + 1] != "^"
-#                     and data[r - 2][c + 1] != "^"
-#                 ):
-#                     node_paths += int(data[r - 2][c + 1])
-#                 data[r][c] = str(node_paths)
-#
-#     for row in data:
-#         print(row)
-#
-#
-# print(f"A single tachyon particle end up on {part_02(cols)} timelines.")
-#
+def part_02(data):
+    """
+    Part 2 logic. Spent lots of time reading about graphs and trees, went crazy
+    for a while trying to figure it out. Eventually, looking at Yang Hui's triangle,
+    it struck me how simple the problem actually was. I used only lists and no
+    recursion which may be considered brutish way to do it and yet it allows the code
+    to calculate 422102272495018 different paths in just 90 milliseconds.
+    """
+
+    count = 0
+    split = 0
+    while count < 12000:
+        for ri, row in enumerate(data):
+            for ci, col in enumerate(row):
+                if ri < len(data) - 1 and col == "|" and data[ri + 1][ci] == " ":
+                    data[ri + 1][ci] = "|"
+                elif col == "^":
+                    if data[ri - 1][ci] == "|":
+                        data[ri][ci + 1] = "|"
+                        data[ri][ci - 1] = "|"
+                        data[ri][ci] = "@"
+                        split += 1
+            count += 1
+
+    # Adding leaf nodes
+    bottom_row = []
+    for r in range(len(data[0])):
+        bottom_row.append(" ")
+    data.append(bottom_row)
+
+    for r, row in enumerate(data):
+        for c, col in enumerate(data[r]):
+            if r == len(data) - 1 and data[r - 1][c] == "|":
+                data[r][c] = "@"
+
+    # Initial count
+    for ri, row in enumerate(cols):
+        for ci, col in enumerate(row):
+            if ri == 2 and col == "@":
+                cols[ri][ci] = "1"
+
+    # Counting ways to each node
+    for r, row in enumerate(data):
+        for c, col in enumerate(row):
+            if data[r][c] == "@":
+                node_paths = 0
+                current_row = r
+                # Adding paths for the whole beam length
+                while data[current_row - 1][c] == "|":
+                    if (
+                        data[current_row - 1][c - 1] != " "
+                        and data[current_row - 1][c - 1] != "|"
+                        and data[current_row - 1][c - 1] != "^"
+                    ):
+                        node_paths += int(data[current_row - 1][c - 1])
+                    if (
+                        c < len(row) - 1
+                        and data[current_row - 1][c + 1] != " "
+                        and data[current_row - 1][c + 1] != "|"
+                        and data[current_row - 1][c + 1] != "^"
+                        and data[current_row - 1][c + 1] != "^"
+                    ):
+                        node_paths += int(data[current_row - 1][c + 1])
+
+                    current_row = current_row - 1
+                data[r][c] = str(node_paths)
+
+    height = len(data)
+    paths = 0
+    for char in data[height - 1]:
+        if char != " ":
+            paths += int(char)
+    return paths
 
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-def dfs_recursive(node):
-    if node is None:
-        print("path!")
-        return
-    print(node.value)
-    dfs_recursive(node.left)
-    dfs_recursive(node.right)
-
-
-node_2 = Node(2)
-node_3 = Node(3)
-node_4 = Node(4)
-node_5 = Node(5)
-
-node_2.left = node_3
-node_2.right = node_4
-node_3.left = node_5
-
-
-dfs_recursive(node_2)
+print(f"A single tachyon particle end up on {part_02(cols)} timelines.")
