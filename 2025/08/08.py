c01e8f0c1d3d74f12fc308d56b21c5282c6d95d3
northernmarshes <northernmarshes@gmail.com>
2026-03-04 14:52:38 +0100

    260304-y25-08-part-1-two-boxes-connected

--- 2025/08/08.py
+++ 2025/08/08.py
@@ -1,22 +1,39 @@
 import numpy as np
+from scipy.spatial import distance
 
 input_file = "input.txt"
 sample_file = "sample.txt"
 
 
-def parse_data(data: str):
-    # space = np.zeros((1000, 1000, 1000))
+def parse_data(data: str) -> np.ndarray:
+    """Parsing the data"""
     box_coordinates = []
     box_coordinates_np = np.zeros
     with open(data, "r") as file:
         for layer in file.readlines():
-            box_coordinates.append(list(layer[:-1].split(",")))
-            box_coordinates_np = np.array(box_coordinates)
+            box_coordinates.append(list(map(int, layer[:-1].split(","))))
+        box_coordinates_np = np.array(box_coordinates)
         return box_coordinates_np
 
 
-print(parse_data(sample_file))
+def part_1(data: np.ndarray):
+    """Part 1 logic"""
+
+    d = distance.squareform(distance.pdist(data))
+    closest = np.argsort(d, axis=1)
+    closest_pairs = closest[:, 0:2]
+    closest_pairs_distances = []
+
+    for pair in closest_pairs:
+        p1 = data[pair[0]]
+        p2 = data[pair[1]]
+        dist = np.linalg.norm(p1 - p2)
+        closest_pairs_distances.append(float(dist))
+    the_closest_pair = closest_pairs[
+        closest_pairs_distances.index(min(closest_pairs_distances))
+    ]
+
+    return the_closest_pair
 
 
-def part_1(data):
-    pass
+print(part_1(parse_data(sample_file)))
