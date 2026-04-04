8c41f3ce46435b7cc2dadf5a61c462bfcc6a4b75
Edna Baud <northernmarshes@gmail.com>
2026-04-04 22:31:32 +0200

    feat(2022/01/part-2): done

---
+++ 2022/01/part_2/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let filename = "input.txt";
+    let mut lines = read_lines(filename);
+
+    // Add empty line to the end of the file.
+    let blank_line = String::from("");
+    lines.push(blank_line);
+
+    let mut calories: u32 = 0;
+    let mut top_three_elves_calories: u32 = 0;
+    let mut each_elfs_calories: Vec<u32> = vec![];
+    let mut three_richest_elves: Vec<u32> = vec![];
+    
+    // Calculate calories of each elf.
+    for line in lines {
+        if line != "" {
+        let value: u32 = line.trim().parse().expect("");
+        calories += &value;
+        } else {
+            each_elfs_calories.push(calories);
+            calories = 0;
+        }
+    }
+    
+    // Sort elves by owned calories.
+    each_elfs_calories.sort();
+
+    // Add three top three amounts.
+    for n in 1..=3 {
+        top_three_elves_calories += each_elfs_calories[each_elfs_calories.len() - n];
+    }
+    
+    println!("The calories from top three elves is {}.", top_three_elves_calories);
+}
+
+/// Change text file into an iterator.
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename).unwrap()
+                            .lines()
+                            .map(String::from)
+                            .collect()
+}
