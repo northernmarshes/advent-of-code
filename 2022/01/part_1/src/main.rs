0a2968f3be977252c3df0c66c4a1d4c6041a57fd
Edna Baud <northernmarshes@gmail.com>
2026-04-04 21:45:41 +0200

    feat(2022/01/part-1): done

--- 2022/01/part_1/src/main.rs
+++ 2022/01/part_1/src/main.rs
@@ -1,10 +1,29 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::read_to_string;
 
 fn main() {
-    let mut f = File::open("sample.txt").expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something wring with the file");
-    println!("{}", contents);
+    let filename = "input.txt";
+    let lines = read_lines(filename);
+    let mut calories: u32 = 0;
+    let mut each_elfs_calories: Vec<u32> = vec![];
+
+    for line in lines {
+        if line != "" {
+        let value: u32 = line.trim().parse().expect("");
+        calories += &value;
+        } else {
+            each_elfs_calories.push(calories);
+            calories = 0;
+        }
+    }
+
+    let max = each_elfs_calories.iter().max().unwrap();
+        println!("The elf that is carrying the most is carrying {} calories.", max);
+}
+
+/// Change text file into an iterator.
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename).unwrap()
+                            .lines()
+                            .map(String::from)
+                            .collect()
 }
