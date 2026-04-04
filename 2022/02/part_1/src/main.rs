9eef300d0eafbbc7f6f0142cc9064ef0c9eb5772
Edna Baud <northernmarshes@gmail.com>
2026-04-05 01:39:07 +0200

    feat(2022/02/part-1): done

--- 2022/02/part_1/src/main.rs
+++ 2022/02/part_1/src/main.rs
@@ -1,17 +1,47 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let filename = "sample.txt";
+    let filename = "input.txt";
     let lines = read_lines(filename);
-    let mut rounds: Vec<(char, char)> = vec![];
-    
-    for line in lines {
-        // let round: Vec<char, char, char> = line.chars().collect();
-        println!("line: {line}");
-        // println!("round: {:?}", round);
-        // rounds.push(round);
+    let mut rounds: Vec<Vec<&str>> = Vec::new();
+    let mut score: u32 = 0;
+
+    // Append rounds as a vector of vectors
+    for line in &lines {
+        let round = line.split(" ").collect::<Vec<&str>>();
+        rounds.push(round);
     }
 
+    // Game logic
+    for round in &rounds{
+        if round[0] == "A" && round[1] == "Y" {
+            score += 2;
+            score += 6;
+        } else if round[0] == "A" && round[1] == "X" {
+            score += 3;
+            score += 1;
+        } else if round[0] == "A" && round[1] == "Z" {
+            score += 3;
+        } else if round[0] == "B" && round[1] == "Y" {
+            score += 3;
+            score += 2;
+        } else if round[0] == "B" && round[1] == "X" {
+            score += 1;
+        } else if round[0] == "B" && round[1] == "Z" {
+            score += 6;
+            score += 3;
+        } else if round[0] == "C" && round[1] == "Y" {
+            score += 2;
+        } else if round[0] == "C" && round[1] == "X" {
+            score += 6;
+            score += 1;
+        } else if round[0] == "C" && round[1] == "Z" {
+            score += 3;
+            score += 3;
+        }
+    }
+
+    println!("The total score would be {score}.");
 }
 
 /// Change text file into an iterator.
