2487976babbe6a6d298cabd71500f0ab05073bf6
Edna Baud <northernmarshes@gmail.com>
2026-04-06 16:39:45 +0200

    feat(2022/03/part-1): wip

--- 2022/03/part_1/src/main.rs
+++ 2022/03/part_1/src/main.rs
@@ -1,15 +1,25 @@
 use std::fs::read_to_string;
+use common_substrings::get_substrings;
 
 fn main() {
-    let mut result:u32 = 0;
+    let result:u32 = 0;
     let filename = "sample.txt";
     let lines: Vec<String> = read_lines(filename);
     let mut common_items: Vec<char> = Vec::new();
 
     for line in &lines {
         let (first_half, second_half) = line.split_at(line.len()/2);
-        println!("{first_half}, {second_half}");
+        let mut rucksacks: Vec<&str> = vec![first_half, second_half];
+        // println!("{rucksacks:?}");
+        let result_substrings = get_substrings(rucksacks, 2, 1);
+        // println!("{:?}, &result_substrings")
+        let item: char = result_substrings[0].name.chars().next().clone().unwrap();
+        // println!("{item}");
+        common_items.push(item);
+        
     }
+
+    println!("{common_items:?}");
 
     println!("{result} is the sum of the priorities of those item types.");
 }
