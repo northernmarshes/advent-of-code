f9dcda19b5cd3bb2d549f99f3b03130efa95cb61
Edna Baud <northernmarshes@gmail.com>
2026-04-06 19:28:15 +0200

    feat(2022/03/part-1): done

--- 2022/03/part_1/src/main.rs
+++ 2022/03/part_1/src/main.rs
@@ -2,24 +2,29 @@ use std::fs::read_to_string;
 use common_substrings::get_substrings;
 
 fn main() {
-    let result:u32 = 0;
-    let filename = "sample.txt";
+    let mut result:u32 = 0;
+    let filename = "input.txt";
     let lines: Vec<String> = read_lines(filename);
     let mut common_items: Vec<char> = Vec::new();
+    let priorities: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+    // Find the common item in both compartments.
     for line in &lines {
         let (first_half, second_half) = line.split_at(line.len()/2);
-        let mut rucksacks: Vec<&str> = vec![first_half, second_half];
-        // println!("{rucksacks:?}");
+        let rucksacks: Vec<&str> = vec![first_half, second_half];
         let result_substrings = get_substrings(rucksacks, 2, 1);
-        // println!("{:?}, &result_substrings")
         let item: char = result_substrings[0].name.chars().next().clone().unwrap();
-        // println!("{item}");
         common_items.push(item);
-        
     }
 
-    println!("{common_items:?}");
+    // Calculate the result by adding corresponding indexes.
+    for item in common_items {
+        let priority: u32 = priorities.iter().position(|&x| x == item).unwrap() as u32;
+        result += priority + 1;
+    }
 
     println!("{result} is the sum of the priorities of those item types.");
 }
