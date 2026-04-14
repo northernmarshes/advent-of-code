09c63320df3db1ac97698de351dca405c26172b1
northernmarshes <northernmarshes@gmail.com>
2026-04-14 02:02:09 +0200

    feat(2022/06/part-1): done

--- 2022/day-06/src/bin/part-1.rs
+++ 2022/day-06/src/bin/part-1.rs
@@ -2,6 +2,9 @@ use day_06::process_part1;
 
 fn main() {
     let file = "./input.txt";
-    // let file = "./sample.txt";
-    println!("{}", process_part1(file));
+    // let file = "./sample_1.txt";
+    let result: String = process_part1(file);
+    println!(
+        "{result} characters need to be processed before the first start-of-packet marker is detected."
+    );
 }
--- 2022/day-06/src/lib.rs
+++ 2022/day-06/src/lib.rs
@@ -1,15 +1,33 @@
-use std::fs::read_to_string;
+use std::{collections::HashSet, fs::read_to_string};
 
-pub fn process_part1(input: &str) {}
-pub fn process_part2(input: &str) {}
+pub fn process_part1(input: &str) -> String {
+    let line: String = read_lines(input);
+    let mut result: u32 = 0;
+    let mut letters: Vec<char> = Vec::new();
+
+    for char in line.chars() {
+        letters.push(char);
+    }
+
+    for (index, _letter) in letters.iter().enumerate() {
+        let mut set = HashSet::new();
+        set.insert(&letters[index]);
+        set.insert(&letters[index + 1]);
+        set.insert(&letters[index + 2]);
+        set.insert(&letters[index + 3]);
+        if set.len() == 4 {
+            result = index as u32 + 4;
+            break;
+        }
+    }
+
+    result.to_string()
+}
+// pub fn process_part2(input: &str) {}
 
 // parse the input
-pub fn read_lines(name: &str) -> Vec<String> {
-    read_to_string(name)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
+pub fn read_lines(name: &str) -> String {
+    read_to_string(name).unwrap()
 }
 
 #[cfg(test)]
@@ -17,11 +35,43 @@ mod tests {
     use super::*;
 
     // mjqjpqmgbljsphdztnvjfqwrcgsmlb
+    // bvwbjplbgvbhsrlpgdmjqwftvncz
+    // nppdvjthqldpwncqszvftbrmjlhg
+    // nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+    // zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
 
     #[test]
-    fn part1_works() {
-        let input = "./sample.txt";
+    fn part1_works_01() {
+        let input = "./sample_1.txt";
         let result = process_part1(input);
         assert_eq!(result, "7");
+    }
+
+    #[test]
+    fn part1_works_02() {
+        let input = "./sample_2.txt";
+        let result = process_part1(input);
+        assert_eq!(result, "5");
+    }
+
+    #[test]
+    fn part1_works_03() {
+        let input = "./sample_3.txt";
+        let result = process_part1(input);
+        assert_eq!(result, "6");
+    }
+
+    #[test]
+    fn part1_works_04() {
+        let input = "./sample_4.txt";
+        let result = process_part1(input);
+        assert_eq!(result, "10");
+    }
+
+    #[test]
+    fn part1_works_05() {
+        let input = "./sample_5.txt";
+        let result = process_part1(input);
+        assert_eq!(result, "11");
     }
 }
