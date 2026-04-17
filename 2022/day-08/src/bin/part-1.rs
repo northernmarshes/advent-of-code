b3254949046ef7f4b2691ae4e4aa8d9d07fdad92
northernmarshes <northernmarshes@gmail.com>
2026-04-17 22:49:43 +0200

    feat(2022/08/part-1): add forest matrix

--- 2022/day-08/src/bin/part-1.rs
+++ 2022/day-08/src/bin/part-1.rs
@@ -1,8 +1,8 @@
 use day_08::process_part1;
 
 fn main() {
-    let file = "./input.txt";
-    // let file = "./sample.txt";
+    // let file = "./input.txt";
+    let file = "./sample.txt";
     let result = process_part1(file);
     println!("{result}");
 }
--- 2022/day-08/src/lib.rs
+++ 2022/day-08/src/lib.rs
@@ -1,12 +1,25 @@
 use std::fs::read_to_string;
 
 pub fn process_part1(input: &str) -> String {
+    let result = 0;
+    let lines = read_lines(input);
+    let mut forest_matrix: Vec<Vec<u8>> = Vec::new();
+    for line in lines {
+        let mut tree_row: Vec<u8> = Vec::new();
+        for c in line.chars() {
+            let tree = c.to_string().parse::<u8>().unwrap();
+            tree_row.push(tree);
+        }
+        forest_matrix.push(tree_row);
+    }
+
+    println!("{forest_matrix:?}");
     result.to_string()
 }
 
-pub fn process_part2(input: &str) -> String {
-    result.to_string()
-}
+// pub fn process_part2(input: &str) -> String {
+// result.to_string()
+// }
 
 // parse the input
 pub fn read_lines(name: &str) -> Vec<String> {
