077158c9efaa4f08278d807e934b7f4f7ec28f30
northernmarshes <northernmarshes@gmail.com>
2026-04-18 12:56:00 +0200

    feat(2022/08/part-1): start main logic

--- 2022/day-08/src/lib.rs
+++ 2022/day-08/src/lib.rs
@@ -2,6 +2,30 @@ use std::fs::read_to_string;
 
 pub fn process_part1(input: &str) -> String {
     let result = 0;
+    let _trees_seen: u32 = 0;
+    let forest = make_matrix(input);
+
+    // println!("{forest:?}");
+
+    // logic
+    for row in forest[1..forest.len() - 1].iter() {
+        for (i, tree) in row[1..row.len() - 1].iter().enumerate() {
+            let previous_highest = row[0..i].iter().max().unwrap_or(&0);
+            // println!("highest {previous_highest}");
+            if tree > previous_highest {
+                // println!("{tree}");
+            }
+        }
+    }
+
+    result.to_string()
+}
+
+// pub fn process_part2(input: &str) -> String {
+// result.to_string()
+// }
+
+pub fn make_matrix(input: &str) -> Vec<Vec<u8>> {
     let lines = read_lines(input);
     let mut forest_matrix: Vec<Vec<u8>> = Vec::new();
     for line in lines {
@@ -12,14 +36,8 @@ pub fn process_part1(input: &str) -> String {
         }
         forest_matrix.push(tree_row);
     }
-
-    println!("{forest_matrix:?}");
-    result.to_string()
+    forest_matrix
 }
-
-// pub fn process_part2(input: &str) -> String {
-// result.to_string()
-// }
 
 // parse the input
 pub fn read_lines(name: &str) -> Vec<String> {
