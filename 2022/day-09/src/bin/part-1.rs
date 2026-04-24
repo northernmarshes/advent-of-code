eeafaeb6e9d1ffe16053e948dc798c8fbd1156a5
northernmarshes <northernmarshes@gmail.com>
2026-04-24 15:22:57 +0200

    feat(2022/09/part-1): add parse_commands

--- 2022/day-09/src/bin/part-1.rs
+++ 2022/day-09/src/bin/part-1.rs
@@ -1,8 +1,8 @@
 use day_09::process_part1;
 
 fn main() {
-    let file = "./input.txt";
-    // let file = "./sample.txt";
+    // let file = "./input.txt";
+    let file = "./sample.txt";
     let result = process_part1(file);
     println!("{result}");
 }
--- 2022/day-09/src/lib.rs
+++ 2022/day-09/src/lib.rs
@@ -1,7 +1,23 @@
 use std::fs::read_to_string;
 
+#[derive(Debug)]
+pub struct Cmd {
+    pub dir: char,
+    pub dis: u8,
+}
+
 pub fn process_part1(input: &str) -> String {
     let result = 0;
+    // let mut height = 0;
+    // let mut width = 0;
+    let raw_commands = read_lines(input);
+    let mut bridge: Vec<Vec<char>> = vec![vec!['O'; 15], vec!['O'; 15]];
+    let mut _positions_map = bridge.clone();
+    let commands = parse_commands(raw_commands);
+    bridge[0][0] = 'H';
+
+    println!("{commands:?}");
+
     result.to_string()
 }
 
@@ -9,6 +25,28 @@ pub fn process_part1(input: &str) -> String {
 //     let result = 0;
 //     result.to_string()
 // }
+
+pub fn parse_commands(cmd: Vec<String>) -> Vec<Cmd> {
+    let mut commands: Vec<Cmd> = Vec::new();
+    for line in cmd {
+        let c = line
+            .split_ascii_whitespace()
+            .next()
+            .unwrap()
+            .chars()
+            .next()
+            .expect("Not found");
+        let d = line
+            .split_ascii_whitespace()
+            .last()
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
+        let cmd = Cmd { dir: c, dis: d };
+        commands.push(cmd);
+    }
+    commands
+}
 
 // parse the input
 pub fn read_lines(name: &str) -> Vec<String> {
