b6d20355e2278f1ff483621bae6d46e1a180844e
northernmarshes <northernmarshes@gmail.com>
2026-04-27 22:26:22 +0200

    feat(2022/09/part-2): struct update works, delete iterator

--- 2022/day-09/src/lib.rs
+++ 2022/day-09/src/lib.rs
@@ -35,44 +35,6 @@ pub struct LongLine {
     pub tail: LinePoint,
 }
 
-impl IntoIterator for LongLine {
-    type Item = LinePoint;
-    type IntoIter = LinePointIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        LinePointIterator {
-            point: self,
-            index: 0,
-        }
-    }
-}
-
-impl Iterator for LinePointIterator {
-    type Item = LinePoint;
-    fn next(&mut self) -> Option<LinePoint> {
-        let result = match self.index {
-            0 => self.point.head,
-            1 => self.point.p_9,
-            2 => self.point.p_8,
-            3 => self.point.p_7,
-            4 => self.point.p_6,
-            5 => self.point.p_5,
-            6 => self.point.p_4,
-            7 => self.point.p_3,
-            8 => self.point.p_2,
-            9 => self.point.tail,
-            _ => return None,
-        };
-        self.index += 1;
-        Some(result)
-    }
-}
-
-pub struct LinePointIterator {
-    point: LongLine,
-    index: usize,
-}
-
 impl Index<&'_ usize> for LongLine {
     type Output = LinePoint;
     fn index(&self, s: &usize) -> &LinePoint {
@@ -149,7 +111,6 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    let result = 0;
     let mut positions: HashSet<(i64, i64)> = HashSet::new();
     let start: (i64, i64) = (0, 0);
     positions.insert(start);
@@ -168,19 +129,18 @@ pub fn process_part2(input: &str) -> String {
         tail: LinePoint { x: 0, y: 0 },
     };
 
-    // testing struct indexing
-    let test = long_line[&3];
-    let test_var = test.x;
-    println!("its a test{test:?} and a var{test_var}");
-
     for command in commands {
-        let (line, _positions) = move_long_rope(long_line, command);
+        let (line, tail_positions) = move_long_rope(long_line, command);
+        for pos in tail_positions {
+            positions.insert(pos);
+        }
         long_line = line;
         let head: (i64, i64) = (long_line.head.y, long_line.head.x);
         println!("The rope's head is at: {head:?}");
         println!("Current rope positions are: {long_line:#?}");
     }
 
+    let result = positions.len();
     result.to_string()
 }
 
@@ -232,7 +192,7 @@ pub fn move_rope(line: &mut ShortLine, command: Cmd) -> (&mut ShortLine, HashSet
 }
 
 pub fn move_long_rope(mut long_line: LongLine, command: Cmd) -> (LongLine, HashSet<(i64, i64)>) {
-    let positions: HashSet<(i64, i64)> = HashSet::new();
+    let mut positions: HashSet<(i64, i64)> = HashSet::new();
     let distance = command.dis;
 
     for _step in 0..distance {
@@ -244,48 +204,37 @@ pub fn move_long_rope(mut long_line: LongLine, command: Cmd) -> (LongLine, HashS
             _ => println!("This ain't a direction!"),
         };
 
-        for (i, point) in long_line.into_iter().enumerate().take(9) {
+        for i in 0..9 {
+            let next_index: usize = &i + 1;
             let first = EndPoint {
-                x: point.x as f64,
-                y: point.y as f64,
+                x: long_line[&i].x as f64,
+                y: long_line[&i].y as f64,
             };
 
             let next = EndPoint {
-                x: long_line.into_iter().nth(i + 1).unwrap().x as f64,
-                y: long_line.into_iter().nth(i + 1).unwrap().y as f64,
+                x: long_line[&next_index].x as f64,
+                y: long_line[&next_index].x as f64,
             };
 
             let rope_points_distance = first.distance_to(&next);
-            // println!("the distance is {rope_points_distance}");
-            //
 
-            // TODO: update positions in the line struct properly
             if rope_points_distance >= 2.0 {
                 if command.dir == 'U' {
-                    long_line.into_iter().nth(i + 1).unwrap().x = point.x;
-                    long_line.into_iter().nth(i + 1).unwrap().y = point.y - 1;
+                    long_line[&next_index].x = long_line[&i].x;
+                    long_line[&next_index].y = long_line[&i].y - 1;
                 } else if command.dir == 'D' {
-                    long_line.into_iter().nth(i + 1).unwrap().x = point.x;
-                    long_line.into_iter().nth(i + 1).unwrap().y = point.y + 1;
+                    long_line[&next_index].x = long_line[&i].x;
+                    long_line[&next_index].y = long_line[&i].y + 1;
                 } else if command.dir == 'R' {
-                    long_line.into_iter().nth(i + 1).unwrap().x = point.x - 1;
-                    long_line.into_iter().nth(i + 1).unwrap().y = point.y;
+                    long_line[&next_index].x = long_line[&i].x - 1;
+                    long_line[&next_index].y = long_line[&i].y;
                 } else {
-                    long_line.into_iter().nth(i + 1).unwrap().x = point.x + 1;
-                    long_line.into_iter().nth(i + 1).unwrap().y = point.y;
+                    long_line[&next_index].x = long_line[&i].x + 1;
+                    long_line[&next_index].y = long_line[&i].y;
                 }
-                // println!("Current point is {point:?} and next point is{n:?}");
+                let position = (long_line[&9].x, long_line[&9].y);
+                positions.insert(position);
             }
-
-            let y = long_line.into_iter().nth(i).unwrap().y;
-            let x = long_line.into_iter().nth(i).unwrap().x;
-            println!("y is {y} and x is {x}");
-
-            let y_n = long_line.into_iter().nth(i + 1).unwrap().y;
-            let x_n = long_line.into_iter().nth(i + 1).unwrap().x;
-            println!("next y is {y_n} and next x is {x_n}");
-            // let n = long_line.into_iter().nth(i + 1).unwrap();
-            // println!("next point is now at {n:?}");
         }
     }
 
