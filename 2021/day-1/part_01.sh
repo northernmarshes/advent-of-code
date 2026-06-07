f3c751fd797e094698aa0f2e2400486d0bb68270
northernmarshes <northernmarshes@gmail.com>
2026-06-08 00:59:04 +0200

    feat(2021/01/part-1): done

---
+++ 2021/day-1/part_01.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# mapfile -t ARR <sample.txt
+mapfile -t ARR <input.txt
+INDEX=-1
+INCREASED=0
+
+for LINE in "${ARR[@]}"; do
+  PREVIOUS="${ARR["$INDEX"]}"
+  echo "Previous: $PREVIOUS"
+  echo "Current: $LINE"
+  if [ $LINE -gt $PREVIOUS ]; then
+    echo "Increased"
+    INCREASED="$(($INCREASED + 1))"
+  else
+    echo "Decreased"
+  fi
+  INDEX="$(($INDEX + 1))"
+done
+
+echo "Increased $INCREASED times."
